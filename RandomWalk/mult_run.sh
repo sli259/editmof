89c18a30ccf9fd2bb956ae338d5f9077cab6f5fe
sli259 <sli259@g.uky.edu>
2020-11-30 15:40:54 -0600

    Add files via upload
    Run script to apply multiple path trajectories. Generate a data file that can be plot with violineplt.py

---
+++ RandomWalk/mult_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir path_file
+
+for i in {1..100}
+    do
+        python walk.py -a 50 -r 3 -n 20
+        cp 2d-walk.dat run-$i.dat
+        awk 'NR==4{print; }' run-$i.dat | awk '{$1=""; print}' >> data
+        mv run-$i.dat ./path_file
+    done
