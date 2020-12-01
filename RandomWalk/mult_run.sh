d982df005a97be14368a9eff20352feb8e62f033
sli259 <sli259@tamu.edu>
2020-12-01 15:34:44 -0600

    Update mult_run.sh

--- RandomWalk/mult_run.sh
+++ RandomWalk/mult_run.sh
@@ -4,7 +4,7 @@ mkdir path_file
 
 for i in {1..100}
     do
-        python walk.py -a 50 -r 3 -n 20
+        python 2D_walk.py -a 50 -r 3 -n 20
         cp 2d-walk.dat run-$i.dat
         awk 'NR==4{print; }' run-$i.dat | awk '{$1=""; print}' >> data
         mv run-$i.dat ./path_file
